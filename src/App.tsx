f4f4e6c3254dfdbaad2fbe172b9b6fc099372c27
Gabriel Rupert Roy Sufrir <officialgabrielsufrir@gmail.com>
2024-02-05 01:31:22 +0800

    style: adjusted main container height

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import CodeEditor from "@/components/code-editor";
 
 function App() {
   return (
-    <div className="p-4 h-[calc(100vh-40px)] flex flex-col gap-2">
+    <div className="p-4 h-[calc(100vh-60px)] flex flex-col gap-2">
       <CodeEditor />
       <div className="flex flex-col text-center">
         <span className="text-sm text-muted-foreground">@gubriyeeel</span>
