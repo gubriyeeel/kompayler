2804a9e1e1ddaedb6854857285ae53ea80bba0bb
Gabriel Rupert Roy Sufrir <officialgabrielsufrir@gmail.com>
2024-02-05 02:07:26 +0800

    fix: added a get function for future use

--- src/api.ts
+++ src/api.ts
@@ -1,15 +1,35 @@
 import axios from "axios";
 import { LANGUAGE_VERSIONS } from "./constants";
 
-const API = axios.create({
+const POST = axios.create({
   baseURL: "https://emkc.org/api/v2/piston",
 });
+
+// * You can get the list of languages here:
+/*
+const GET = axios.get(
+  "https://emkc.org/api/v2/piston/runtimes",
+  {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }
+);
+
+export const getLanguages = async () => {
+  const response = await GET;
+  console.log(response.data);
+  return response.data;
+}
+
+getLanguages();
+*/
 
 export const executeCode = async (sourceCode: string, language: string) => {
   // @ts-ignore
   const version = LANGUAGE_VERSIONS[language];
   // @ts-ignore
-  const response = await API.post("/execute", {
+  const response = await POST.post("/execute", {
     language: language,
     version,
     files: [
